4208184a412c4e1f36519b521d432afb82b3f985
Surabhi Jain <Surabhi.Jain@philips.com>
2024-09-03 23:07:13 +0530

    simpleMonitorChange

--- monitor.cpp
+++ monitor.cpp
@@ -3,36 +3,43 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+void displayCriticalAlert(const std::string& message) {
+    cout << message << "\n";
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
+
+bool isTemperatureCritical(float temperature) {
+    return temperature > 102 || temperature < 95;
+}
+
+bool isPulseRateOutOfRange(float pulseRate) {
+    return pulseRate < 60 || pulseRate > 100;
+}
+
+bool isSpo2OutOfRange(float spo2) {
+    return spo2 < 90;
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    if (isTemperatureCritical(temperature)) {
+        displayCriticalAlert("Temperature is critical!");
+        return 0;
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    if (isPulseRateOutOfRange(pulseRate)) {
+        displayCriticalAlert("Pulse Rate is out of range!");
+        return 0;
     }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    if (isSpo2OutOfRange(spo2)) {
+        displayCriticalAlert("Oxygen Saturation out of range!");
+        return 0;
     }
-    return 0;
-  }
-  return 1;
+    return 1;
 }
